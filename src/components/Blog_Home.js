e54a1701c37965389d95d6887413ce4cce79c52d
bugixtix <113024167+bugixtix@users.noreply.github.com>
2022-12-05 08:26:00 +0000

    " #05"

--- src/components/Blog_Home.js
+++ src/components/Blog_Home.js
@@ -1,21 +1,41 @@
 import React from "react";
 import { Navbar } from "./Home_";
+import { dynamicStyle$ } from "../lainy";
+import './style.css'
 
 export default function B_H (prop) {
 
     var outDiv_s = {
-        minHeight:'100vh',
+        // minHeight:'100vh',
         width:'100vw',
         overflow:'hidden',
+        display:'flex',
+        flexDirection:'column',
     }
     var navDiv_ = {
         position:'fixed',
         width:'100%',
     }
     var outDiv_1 = {
-        backgroundImage:`url('https://github.com/bugixtix/Portfolio/issues/1#issue-1475761779')`,
+        background:`${dynamicStyle$('linear-gradient(#000,#111)',!prop.darkMode_$,'linear-gradient(#e66465, #9198e5)')},url(${require('../imges/wallpaper_.png')})`,
         width:'100%',
-        heigh:'100%',
+        height:'90vh',
+        backgroundPosition:'left',
+        display:'flex',
+        flexDirection:'column',
+        backgroundBlendMode:'saturation'
+    }
+    var h2_s = {
+        textAlign:'center'
+    }
+    var p_s = {
+        textAlign:'center'
+    }
+    var line_ = {
+        padding:'20px 0px',
+        height:'1px',
+        width:'100%',
+        
     }
 
     return(
@@ -24,8 +44,11 @@ export default function B_H (prop) {
             <div style={navDiv_}>
             <Navbar darkMode_$={prop.darkMode_$} setDarkMode_$={prop.setDarkMode_$} winWidth_$={prop.winWidth_$}/>
             </div>
-            <div style={outDiv_1}>
 
+            <div style={outDiv_1}>
+                <h2 style={h2_s}>title you want</h2>
+                <p style={p_s}>description you want</p>
+                <p style={line_}></p>
             </div>
         </div>
     )
